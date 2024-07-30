adb6ba108dc0568aff3bd3d64251d047788d5f68
Philip Maphey <pamaph20@stlawu.edu>
2024-07-30 12:39:56 -0400

    Added Searching

---
+++ homes-app/src/app/user-page/user-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-page.component.html',
+  styleUrls: ['./user-page.component.css']
+})
+export class UserPageComponent {
+
+}
